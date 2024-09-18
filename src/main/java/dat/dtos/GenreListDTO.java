2ee9875c2d798db27a8975ee48cad451232c6a2c
BekhanM <therealbekhan@gmail.com>
2024-09-18 10:06:19 +0200

    genres ikke null længere, moviedto ændret og movie service ny metode added og inkorporeret i MovieService.getDanishMovieFrom2019Plus

---
+++ src/main/java/dat/dtos/GenreListDTO.java
@@ -0,0 +1,14 @@
+package dat.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenreListDTO {
+    private List<GenreDTO> genres;
+}
